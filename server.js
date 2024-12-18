0d7607669d2962f55980a25e6964d0d13e92bd1e
ARPAN CHOWDHURY <77331658+Arpan1908@users.noreply.github.com>
2024-12-18 22:05:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 // app.use(cors({
